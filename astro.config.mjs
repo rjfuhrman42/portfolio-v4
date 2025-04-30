be9584bd846bd64799955b9e12967d4a8dc6951e
rjfuhrman42 <reidjf77@gmail.com>
2025-04-29 21:01:03 -0400

    feat: blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+
   trailingSlash: "always",
+  integrations: [mdx()],
 });
